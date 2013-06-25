eb0b0ec7039781a908bc991303f4ae3400946136
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-06-25 20:21:44 +0200

    updated to cmssw_4_2_8

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 export PYTHONPATH=$PWD:$PYTHONPATH
 #use python2.6 from CMSSW
-if [ -e "/net/software_cms/slc5_ia32_gcc434/external/python/2.6.4-cms2/etc/profile.d/init.sh" ]; then
-  source /net/software_cms/slc5_ia32_gcc434/external/python/2.6.4-cms2/etc/profile.d/init.sh
-  source /net/software_cms/slc5_ia32_gcc434/external/openssl/0.9.7m/etc/profile.d/init.sh
+if [ -e "/net/software_cms/$SCRAM_ARCH/external/python/2.6.4-cms16/etc/profile.d/init.sh" ]; then
+  source /net/software_cms/$SCRAM_ARCH/external/python/2.6.4-cms16/etc/profile.d/init.sh
+  source /net/software_cms/$SCRAM_ARCH/external/openssl/0.9.8e-cms5/etc/profile.d/init.sh
 fi
