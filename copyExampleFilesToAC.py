f936588a6e0ddae4798ec6c604a27c68591d5c88
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-06-27 19:50:53 +0200

    first version of copy script finished

--- copyExampleFilesToAC.py
+++ copyExampleFilesToAC.py
@@ -1,27 +1,7 @@
 #!/usr/bin/env python
 import GridTools.DASclient.das_client as das_client
 import urllib2,json
-from PhEDExDatasvcInfo.PhEDExDatasvcInfo import PhEDExDatasvcInfo
-
-def getCMSName(firstSite):
-  cert = '/home/home2/institut_3b/hoehle/.globus/usercert.pem'
-  ckey    = '/home/home2/institut_3b/hoehle/.globus/userkey.pem'
-  hdlr = das_client.HTTPSClientAuthHandler(ckey,cert)
-  opener = urllib2.build_opener(hdlr)
-  headers = {"Accept": "application/json"}
-  reqSiteRes  = urllib2.Request('https://cmsweb.cern.ch/sitedb/data/prod/site-resources', headers=headers)
-  fdescSiteRes = opener.open(reqSiteRes)
-  data = fdescSiteRes.read()
-  fdesc = opener.open(req)
-  dataSiteRes = fdescSiteRes.read()
-  fdescSiteRes.close()
-  jsondictSiteRes = json.loads(dataSiteRes)
-  siteRes = jsondictSiteRes.get('result')
-  #jsondict.keys()
-  #jsondict.get('desc')
-  for res in siteRes:
-    if firstSite in res[2] and 'SE' in res[1]:
-      print(res[0])
+import PhEDExDatasvcInfo.PhEDExDatasvcInfo as PhEDExDatasvcInfo
 ########
 optmgr  = das_client.DASOptionParser()
 opts, _ = optmgr.get_opt()
@@ -35,19 +15,43 @@ ckey    = opts.ckey
 cert    = opts.cert
 das_h   = opts.das_headers
 base    = opts.base
-
 #############
 import sys,re,subprocess,os
 print "called with ",sys.argv
 file = open(sys.argv[1])
-lines = [ l.strip('\n') for l in file.readlines()][:2]
-phedex = PhEDExDatasvcInfo()
-for line in lines:
+lines = [ l.strip('\n') for l in file.readlines()]
+phedex = PhEDExDatasvcInfo.PhEDExDatasvcInfo()
+srmTimeout = 30
+targetAc='srm://grid-srm.physik.rwth-aachen.de:8443/srm/managerv2?SFN=/pnfs/physik.rwth-aachen.de/cms/store/user/fhohle//OfficialSamples'
+maxNum = len(lines)
+failed = []
+for i,line in enumerate(lines):
   #output = ''
+  print i,"/",maxNum," ",line
   if not line.startswith('#'):
     filename = line.split()[2]
+    datasetname = line.split()[0]
+    print filename,"  ",datasetname
     #print filename
     jsondict = das_client.get_data(host, "site file = "+filename, idx, limit, debug, thr, ckey, cert)
-    node = str(jsondict.get('data')[0].get('site')[0].get('name'))
-    srmPath = phedex.GetPFNFromLFN(node=node,lfn=filename)
-    print srmPath
+    nodes = [ str(ele.get('site')[0].get('name')) for ele in jsondict.get('data')]
+    errorcode = "not done"
+    for node in nodes:
+      print "trying to download from ",node
+      print filename
+      srmPath = phedex.GetPFNFromLFN(node=node,lfn=filename)
+      foldername = re.sub('\/','__',datasetname.lstrip('\/'))
+      filenameNew =  re.match('.*\/([^\/]*\.root)',filename).group(1)
+      print srmPath
+      lcgCpCommand = "lcg-cp -v --srm-timeout "+str(srmTimeout)+" "+srmPath+" "+targetAc+"/"+foldername+"/"+filenameNew
+      print lcgCpCommand
+      output = subprocess.Popen([lcgCpCommand],shell=True,stdout=subprocess.PIPE,env=os.environ)
+      output.wait()
+      errorcode = output.returncode
+      if errorcode == 0:
+        break
+    if errorcode != 0:
+      failed.append(line)
+      print "WARNING FAILED errorcode ",errorcode," ",filename
+for fail in failed:
+  print "failed ",fail
